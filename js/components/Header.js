2fd59d6b38c216c9abb850e413a5e240f860554b
grromovich <Legendaryperson@yandex.ru>
2023-05-25 20:24:09 +0300

    Очень продуктивная работа

--- js/components/Header.js
+++ js/components/Header.js
@@ -3,8 +3,8 @@ let headercomp = Vue.component("header-comp", {
                     <div class="header__container">
                         <div class="logo">Logo</div>
                         <ul class="menu">
-                            <li><a href="">Товары</a></li>
-                            <li><a href="">Блог</a></li>
+                            <li><a href="index.html">Товары</a></li>
+                            <li><a href="blog.html">Блог</a></li>
                         </ul>
                     </div>
                 </div>`
