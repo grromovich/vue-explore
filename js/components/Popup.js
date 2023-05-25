8f04cd444aaf7e94391f39f471db4ee66fde4d0c
grromovich <Legendaryperson@yandex.ru>
2023-05-25 19:29:57 +0300

    Привел в порядок master

---
+++ js/components/Popup.js
@@ -0,0 +1,12 @@
+let popupcomp = Vue.component("popup", {
+    props: {
+        popup: {}
+    },
+    template:  `<div class="overlay" v-if="popup">
+                    <div class="popup">
+                        <div class="krest" @click ="$emit('close', false)">x</div>
+                        <div class="popup__container"><h1>йцу</h1></div>
+                    </div>
+                </div>`
+}
+)
