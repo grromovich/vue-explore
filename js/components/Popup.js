2d2679c68f86a0d3af60f4e338dc6cad68ccf155
grromovich <Legendaryperson@yandex.ru>
2023-05-25 19:37:26 +0300

    На странице отображается только 10 элементов

--- js/components/Popup.js
+++ js/components/Popup.js
@@ -4,7 +4,7 @@ let popupcomp = Vue.component("popup", {
     },
     template:  `<div class="overlay" v-if="popup">
                     <div class="popup">
-                        <div class="krest" @click ="$emit('close', false)">x</div>
+                        <div class="krest" @click ="$emit('close')">x</div>
                         <div class="popup__container"><h1>йцу</h1></div>
                     </div>
                 </div>`
