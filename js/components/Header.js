fa1987957420f02bc56eb706c7e80d3d7154cc34
grromovich <Legendaryperson@yandex.ru>
2023-05-25 20:13:42 +0300

    Перевел хеддер в компонент

---
+++ js/components/Header.js
@@ -0,0 +1,12 @@
+let headercomp = Vue.component("header-comp", {
+    template:  `<div class="header">
+                    <div class="header__container">
+                        <div class="logo">Logo</div>
+                        <ul class="menu">
+                            <li><a href="">Товары</a></li>
+                            <li><a href="">Блог</a></li>
+                        </ul>
+                    </div>
+                </div>`
+}
+)
