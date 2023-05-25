b01f12b896b7883b46d3e6c16fb320a53f1ef742
4godaverstki <teisiq@mail.ru>
2023-05-25 20:04:45 +0300

    add text-popup

--- js/components/GoodList.js
+++ js/components/GoodList.js
@@ -5,7 +5,7 @@ let goodList = Vue.component("good-list",{
     template: `<div><div class="good" v-for="good in goodlist">
                     <div class="good__container" >
                         <img :src="good['images']" alt="">
-                        <h3 class="title">{{good['name']}}</h3>
+                        <h3 class="title" @click ="$emit('open')">{{good['name']}}</h3>
                         <div class="price">{{good['price']}}$</div>
                     </div>
                 </div>
